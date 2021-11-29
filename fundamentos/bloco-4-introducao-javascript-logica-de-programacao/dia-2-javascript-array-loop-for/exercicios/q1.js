6d29122d50bec751ad58d34766926384a1dcf6f4
PAULA RIBEIRO <paulasalinor@gmail.com>
2021-11-29 16:03:06 -0300

    adiciona resoluçao questao 4

--- fundamentos/bloco-4-introducao-javascript-logica-de-programacao/dia-2-javascript-array-loop-for/exercicios/q1.js
+++ fundamentos/bloco-4-introducao-javascript-logica-de-programacao/dia-2-javascript-array-loop-for/exercicios/q1.js
@@ -22,3 +22,12 @@ let average = (sum / numbers.length);
 
 console.log(average);
 
+// Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20"
+
+if(average > 20){
+    console.log("valor maior que 20");
+} else{
+    console.log("valor menor que 20");
+}
+
+//
