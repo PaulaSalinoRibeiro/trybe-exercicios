2b8ee9ee922159a71f38ff5811f41622481132d8
PAULA RIBEIRO <paulasalinor@gmail.com>
2021-11-29 15:57:04 -0300

    adiciona resoluçao das questoes 1, 2 e 3

---
+++ fundamentos/bloco-4-introducao-javascript-logica-de-programacao/dia-2-javascript-array-loop-for/exercicios/q1.js
@@ -0,0 +1,20 @@
+// Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() 
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+/* for(let number of numbers){
+    console.log(number);
+} */
+
+// Para o segundo exercício, some todos os valores contidos no array e imprima o resultado
+
+/* let sum = 0;
+
+for(let number of numbers){
+     sum += number;
+}
+
+console.log(sum); */
+
+// Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array
+
