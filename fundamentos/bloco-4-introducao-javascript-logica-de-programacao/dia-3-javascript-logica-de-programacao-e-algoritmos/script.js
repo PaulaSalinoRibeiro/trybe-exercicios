625125bf31a3a0bd36f6f9195cc928124e0119ef
PAULA RIBEIRO <paulasalinor@gmail.com>
2021-11-30 16:12:21 -0300

    simplifica a resoluçao da questao 2

--- fundamentos/bloco-4-introducao-javascript-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-4-introducao-javascript-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -14,17 +14,24 @@ for(let index = 1; index <= n; index += 1){
 
 // 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
 
+/* let n = 5;
+let asterisco = '*';
+let string = ''; 
+
+   for(let index = 1; index <= n; index += 1){
+    string += asterisco;
+    console.log(string);
+   } */
+
+// 3- Agora inverta o lado do triângulo.
+
 let n = 5;
 let asterisco = '*';
 let string = ''; 
 
-for(let index = 1; index <= n; index += 1){
-    for(let index = 1; index <= n; index += 1){
-    }
+   for(let index = 1; index <= n; index += 1){
     string += asterisco;
     console.log(string);
-}
-
-// 3- Agora inverta o lado do triângulo.
+   }
 
 // 4- Depois, faça uma pirâmide com n asteriscos de base
