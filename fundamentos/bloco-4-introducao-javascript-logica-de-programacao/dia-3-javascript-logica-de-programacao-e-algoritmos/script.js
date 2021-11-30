242d126ba67d251ec3af6672c63ec8805c7ad597
PAULA RIBEIRO <paulasalinor@gmail.com>
2021-11-30 19:39:15 -0300

    adiciona comentarios a questao 1

--- fundamentos/bloco-4-introducao-javascript-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-4-introducao-javascript-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -4,12 +4,12 @@
 let asterisco = '*';
 let string = ''; 
 
-for(let index = 1; index <= n; index += 1){
+for(let index = 1; index <= n; index += 1){ // imprime os * em linha dentro de uma string
   string += asterisco;
 }
 
-for(let index = 1; index <= n; index += 1){
-    console.log(string);
+for(let index = 1; index <= n; index += 1){ // imprime os * em coluna
+    console.log(string); 
 } */
 
 // 2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
@@ -21,20 +21,26 @@ let string = '';
    for(let index = 1; index <= n; index += 1){
     string += asterisco;
     console.log(string);
-   } */
-
+   }
+ */
 // 3- Agora inverta o lado do triângulo.
+
+
 
 // 4- Depois, faça uma pirâmide com n asteriscos de base
 
+
+
 // 5- Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor de n será sempre ímpar.
+
+
 
 // 6- Faça um programa que diz se um número definido numa variável é primo ou não;
 
 // você vai precisar de fazer um loop que vá de 0 ao número definido
 // além disso, vai precisar de fazer uma checagem a cada iteração e armazenar os resultados em algum lugar.
 
-let number = 50;
+/* let number = 50;
 let arrNumbersPrime = [];
 
 for(let index = 0; index <= number; index += 1){
@@ -47,5 +53,5 @@ for(let index = 0; index < arrNumbersPrime.length; index += 1){
     if(arrNumbersPrime[index] === number){
         console.log("Is prime number")
     }
-}
+} */
 
