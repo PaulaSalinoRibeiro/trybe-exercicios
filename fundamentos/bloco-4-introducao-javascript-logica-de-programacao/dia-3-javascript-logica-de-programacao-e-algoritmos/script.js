0c24e09f25a020c2f6c3cf1c2f072340bfa7647a
PAULA RIBEIRO <paulasalinor@gmail.com>
2021-11-30 18:09:15 -0300

    adiciona resoluçao da questao 6

--- fundamentos/bloco-4-introducao-javascript-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
+++ fundamentos/bloco-4-introducao-javascript-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/script.js
@@ -25,13 +25,27 @@ let string = '';
 
 // 3- Agora inverta o lado do triângulo.
 
-let n = 5;
-let asterisco = '*';
-let string = ''; 
-
-   for(let index = 1; index <= n; index += 1){
-    string += asterisco;
-    console.log(string);
-   }
-
 // 4- Depois, faça uma pirâmide com n asteriscos de base
+
+// 5- Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor de n será sempre ímpar.
+
+// 6- Faça um programa que diz se um número definido numa variável é primo ou não;
+
+// você vai precisar de fazer um loop que vá de 0 ao número definido
+// além disso, vai precisar de fazer uma checagem a cada iteração e armazenar os resultados em algum lugar.
+
+let number = 50;
+let arrNumbersPrime = [];
+
+for(let index = 0; index <= number; index += 1){
+    if(index % 2 !== 0){
+        arrNumbersPrime.push(index);
+    }
+}
+
+for(let index = 0; index < arrNumbersPrime.length; index += 1){
+    if(arrNumbersPrime[index] === number){
+        console.log("Is prime number")
+    }
+}
+
