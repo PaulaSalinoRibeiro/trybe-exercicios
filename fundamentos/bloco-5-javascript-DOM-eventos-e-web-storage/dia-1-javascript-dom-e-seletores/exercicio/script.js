e114c7ff38bd6ec226b1413d4717a866847e28bd
PAULA RIBEIRO <paulasalinor@gmail.com>
2021-12-03 18:28:22 -0300

    cria funçoes para os requesitos 1, 2 e 3

---
+++ fundamentos/bloco-5-javascript-DOM-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/exercicio/script.js
@@ -0,0 +1,34 @@
+/* Aqui você vai modificar os elementos já existentes utilizando apenas as funções:
+- document.getElementById()
+- document.getElementsByClassName()
+- document.getElementsByTagName()
+ok Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
+ok Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
+ok Crie uma função que mude a cor do quadrado vermelho para branco.
+Crie uma função que corrija o texto da tag <h1>.
+Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
+Crie uma função que exiba o conteúdo de todas as tags <p> no console.
+ */
+
+
+function changeParagraph (text){
+    let changeParagraph = document.querySelectorAll('.center-content p');
+    changeParagraph[1].innerText = text;   
+}
+
+changeParagraph('xablau');
+
+function changeColor(color){
+    let changeColor = document.getElementsByClassName('main-content');
+    changeColor[0].style.backgroundColor = color;
+}
+
+changeColor('rgb(76,164,109)');
+
+function otherColor (color){
+    let otherColor = document.querySelector('.center-content');
+    otherColor.style.backgroundColor = color;
+
+}
+
+otherColor('white');
---
+++ fundamentos/bloco-5-javascript-DOM-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,16 @@
+let header = document.getElementById("header-container");
+
+header.style.textAlign = "center";
+header.style.color = "white";
+header.style.backgroundColor = "#11a36a";
+header.style.padding = "5px";
+
+let div = document.querySelectorAll('div');
+
+div[0].style.backgroundColor = "tomato";
+div[0].style.display = "inline-block";
+div[0].style.height = "150px";
+div[0].style.width = "300px";
+div[0].style.marginLeft = "500px";
+
+
