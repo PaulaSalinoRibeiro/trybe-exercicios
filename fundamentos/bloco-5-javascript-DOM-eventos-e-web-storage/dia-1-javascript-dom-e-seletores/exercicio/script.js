5a2691976309f431068f84b8bd2a8fef58bda802
PAULA RIBEIRO <paulasalinor@gmail.com>
2021-12-03 18:51:07 -0300

    cria funcao que modifica para maiusculo todas as tags p

--- fundamentos/bloco-5-javascript-DOM-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/exercicio/script.js
+++ fundamentos/bloco-5-javascript-DOM-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/exercicio/script.js
@@ -5,8 +5,8 @@
 ok Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
 ok Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
 ok Crie uma função que mude a cor do quadrado vermelho para branco.
-Crie uma função que corrija o texto da tag <h1>.
-Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
+ok Crie uma função que corrija o texto da tag <h1>.
+ok Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
 Crie uma função que exiba o conteúdo de todas as tags <p> no console.
  */
 
@@ -40,3 +40,17 @@ function checkOrtg(text){
 }
 
 checkOrtg('Exercício 5.1 - Javascript');
+
+  function upperCaseTagParagraph(){
+    let paragraphs = document.querySelectorAll('p');
+
+    for(let p = 0; p < paragraphs.length; p += 1){
+        paragraphs[p].innerText = paragraphs[p].innerText.toUpperCase()
+    }
+
+    return paragraphs
+}
+
+upperCaseTagParagraph(); 
+
+
