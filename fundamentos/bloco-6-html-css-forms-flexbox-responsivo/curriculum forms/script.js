70aa50fb55748f2efb4c1a9b99728742218d2330
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-01-04 18:50:02 -0300

    coloca as tags do form dentro de divs

---
+++ fundamentos/bloco-6-html-css-forms-flexbox-responsivo/curriculum forms/script.js
@@ -0,0 +1,11 @@
+let states = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins']
+
+let setect = document.getElementById('states');
+
+for(let i = 0; i < states.length; i += 1){
+    let option = document.createElement('option');
+    option.innerText = states[i]
+    option.value = states[i]
+    setect.appendChild(option); 
+}
+
