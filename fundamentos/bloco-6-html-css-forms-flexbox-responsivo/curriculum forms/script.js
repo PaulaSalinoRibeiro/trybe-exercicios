eaa642c0236c7e5f066f33aa9aa603b03a8e7131
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-01-06 13:46:16 -0300

    adiciona Just-validate para validaçao dos campos

--- fundamentos/bloco-6-html-css-forms-flexbox-responsivo/curriculum forms/script.js
+++ fundamentos/bloco-6-html-css-forms-flexbox-responsivo/curriculum forms/script.js
@@ -1,11 +1,59 @@
-let states = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins']
 
-let setect = document.getElementById('states');
+function statesOptions () {
 
-for(let i = 0; i < states.length; i += 1){
-    let option = document.createElement('option');
-    option.innerText = states[i]
-    option.value = states[i]
-    setect.appendChild(option); 
+    let states = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins']
+    let setect = document.getElementById('states');
+    
+    for(let i = 0; i < states.length; i += 1){
+        let option = document.createElement('option');
+        option.innerText = states[i]
+        option.value = states[i]
+        setect.appendChild(option); 
+    }
+
 }
 
+function defaultSubmit (event) {
+    event.preventDefault()
+}
+
+const validate = new window.JustValidate('#form');
+
+validate
+  .addField('#name', [
+    {
+      rule: 'required',
+    },
+    {
+      rule: 'maxLength',
+      value: 40,
+    },
+  ])
+  .addField('#email', [
+    {
+        rule: 'required',
+    },
+    {
+        rule: 'maxLength',
+        value: 50,
+    }
+  ])
+  .addField('#cpf', [ // cpf accept just numbers
+      {
+        rule: 'required',
+      },
+      {
+        rule: 'maxLength',
+         value: 11
+      }
+  ]) 
+  
+
+  window.onload = function () {
+
+    statesOptions ();
+
+    let btnSubmit = document.getElementById('btn-submit');
+    btnSubmit.addEventListener('click', defaultSubmit);
+
+}
