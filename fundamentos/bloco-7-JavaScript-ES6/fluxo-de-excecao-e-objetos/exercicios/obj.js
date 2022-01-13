8f65ee57f777f463ca8e76b97d42c28a8d0cd52a
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-01-13 18:53:24 -0300

    parte III - metodos de objetos

--- fundamentos/bloco-7-JavaScript-ES6/fluxo-de-excecao-e-objetos/exercicios/obj.js
+++ fundamentos/bloco-7-JavaScript-ES6/fluxo-de-excecao-e-objetos/exercicios/obj.js
@@ -46,9 +46,69 @@ const orderModifier = (order) => {
   
   order.name = 'Luiz Silva';
   order.payment.total = 50;
+  const arrPizza = Object.keys(order.order.pizza);
 
-  return `Olá ${order.name}, o total do seu pedido de margerita e pepperoni e ${order.order.drinks.coke.type} é R$${order.payment.total}`
+  return `Olá ${order.name}, o total do seu pedido de ${arrPizza[0]} e ${arrPizza[1]} e ${order.order.drinks.coke.type} é R$${order.payment.total}`
 
 }
 
-console.log(orderModifier(order));
+orderModifier(order);
+
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const lessos2AND3 =  Object.assign({}, lesson2, lesson3);
+
+const keyList = obj => {
+  
+  const arr = Object.keys(obj);
+  for(const key of arr){
+    console.log(key);
+ }
+
+}
+
+const objLength = obj => Object.keys(obj).length;
+
+const valueList = obj => {
+  
+  const arr = Object.values(obj);
+  for(const key of arr){
+    console.log(key);
+ }
+
+}
+
+const allLessons = Object.assign({}, {lesson1, lesson2, lesson3});
+
+
+const allStudent = obj => {
+
+  const arr = Object.entries(obj);
+  let sum = 0;
+
+  for (const student of arr){
+    sum += student[1].numeroEstudantes;
+  }
+
+  return sum;
+}
+
+console.log(allStudent(allLessons));
