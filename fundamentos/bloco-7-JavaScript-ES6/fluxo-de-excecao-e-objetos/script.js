148e16a5c4e45984633b5824087d690c5a2c27e2
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-01-13 07:39:20 -0300

    objeto: adicionando novas chaves

--- fundamentos/bloco-7-JavaScript-ES6/fluxo-de-excecao-e-objetos/script.js
+++ fundamentos/bloco-7-JavaScript-ES6/fluxo-de-excecao-e-objetos/script.js
@@ -17,3 +17,41 @@ const sum = (value1, value2) => {
   }
 }
 
+//console.log(sum(2, '3'));
+
+const customer = {
+  firstName: 'Roberto',
+  age: 22,
+  job: 'Teacher',
+}
+
+customer.lastName = 'Farta';
+customer['adress'] = 'Rua torta n° 0';
+
+//console.log(customer)
+
+const verifyTypes = (obj, key, value) => {
+
+  if(typeof obj !== 'object' || typeof key !== 'string'){
+    throw new Error('Valores inválidos');
+  }
+}
+
+const creatObj = (obj, key, value) => {
+
+  try {
+    verifyTypes(obj, key, value);
+    obj[key] = value;
+    return obj;
+
+  } catch (erro) {
+    throw erro.message
+  }
+  
+}
+
+let obj = {
+  firstName: 'Paula',
+}
+
+//console.log(creatObj(obj, 'age', 31))
