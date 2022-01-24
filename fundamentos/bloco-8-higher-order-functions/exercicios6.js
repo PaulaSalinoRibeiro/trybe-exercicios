502b6257d20fdb4adaa5ab8176634b60a934f21c
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-01-24 10:05:08 -0300

    exercicio 6

--- fundamentos/bloco-8-higher-order-functions/exercicios6.js
+++ fundamentos/bloco-8-higher-order-functions/exercicios6.js
@@ -6,12 +6,19 @@ const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
 const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 
 function studentAverage() {
-  // escreva seu código aqui
+  return students.map((student, index) => ({
+    name: student,
+    averege: grades[index].reduce((acc, curr) => {
+      return (acc + curr / grades[index].length);
+    }, 0).toFixed(1)
+  }))
+  
 }
 
-const expected = [
+/* const expected = [
   { name: 'Pedro Henrique', average: 7.8 },
   { name: 'Miguel', average: 9.2 },
   { name: 'Maria Clara', average: 8.8 },
-];
+]; */
 
+console.log(studentAverage());
