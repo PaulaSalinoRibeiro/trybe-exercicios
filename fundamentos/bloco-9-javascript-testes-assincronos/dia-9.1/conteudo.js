68b238c9508d88c7169f2185bef206aacf4248ad
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-01-28 12:31:24 -0300

    conteudo do dia 9.1

---
+++ fundamentos/bloco-9-javascript-testes-assincronos/dia-9.1/conteudo.js
@@ -0,0 +1,211 @@
+// --> Operações assincronas
+
+/* console.log('1 - Receber roda');
+console.log('2 - Encaixar parafusos');
+console.log('3 - Fixar roda no carro'); */
+
+/* console.log('1 - Comprar parafusos');
+console.log('2 - Adicionar ao estoque');
+console.log('3 - Receber roda');
+console.log('4 - Encaixar parafusos');
+console.log('5 - Fixar roda no carro'); */
+
+/* setTimeout(() => {
+  console.log('Comprar parafusos') // Comprar parafusos
+  console.log('Adicionar ao estoque') // Adicionar ao estoque
+}, 2000);
+
+console.log('1 - Receber roda'); // 1 - Receber roda
+console.log('2 - Encaixar parafusos'); // 2 - Encaixar parafusos
+console.log('3 - Fixar roda no carro'); // 3 - Fixar roda no carro */
+
+/* const pushNumber = (numbers, number) => numbers.push(number);
+
+let numbers = [];
+
+pushNumber(numbers, 1);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+console.log(numbers); */
+
+/* const pushNumber = (list, number) => list.push(number);
+
+let numbers = [];
+
+setTimeout(() => pushNumber(numbers, 1), 3000);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+console.log(numbers); // O retorno é [2, 3] */
+
+/* const pushNumber = (list, number) => list.push(number);
+
+let numbers = [];
+
+pushNumber(numbers, 1);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+setTimeout(() => console.log(numbers), 3000); */
+
+/* const pushNumber = (list, number) => list.push(number);
+
+let numbers = [];
+
+setTimeout(() => pushNumber(numbers, 1), 3000);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+setTimeout(() => console.log(numbers), 3000); */
+
+// https://www.w3schools.com/jsref/met_win_settimeout.asp
+
+
+// --> Callbacks
+
+/* const despesas = [
+  {
+    gym: 99,
+  },
+  {
+    ifood: 200,
+  },
+  {
+    phone: 60,
+  },
+  {
+    internet: 100,
+  },
+];
+
+const renda = 1000;
+
+const despesaMensal = (renda, despesas, callback) => {
+
+  const despesaTotal = callback(despesas);
+  const saldoFinal = renda - despesaTotal;
+
+  console.log(`Balanço do mês:
+    Recebido: R$${renda},00
+    Gasto: R$${despesaTotal},00
+    Saldo: R$${saldoFinal},00 `);
+};
+
+const somaDespesas = (despesas) => {
+  const custoItem = despesas.map((item) => Object.values(item)); // [ [ 99 ], [ 200 ], [ 60 ], [ 100 ] ]
+  // console.log(custoItem);
+  const despesaTotal = custoItem.reduce((acc, curr) => acc += curr[0], 0);
+  return despesaTotal;
+};
+
+// acc é a sigla para accumulator (acumulador)
+// curr é a sigla para current (valor atual)
+
+despesaMensal(renda, despesas, somaDespesas); */
+
+/* // Definição da função userFullName
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+
+// Definição da função getUser
+const getUser = (param) => {
+  const userToReturn = {
+    firstName: "Ivan",
+    lastName: "Ivanovich",
+    nationality: "Russian"
+  };
+  // Retornamos nosso parâmetro, que será uma função (callback)
+  return param(userToReturn);
+};
+
+// Chamada/execução da função getUser, que vai receber como parâmetro nossa função userFullName.
+console.log(getUser(userFullName)); */
+
+/* const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const getUser = (callback) => {
+  const userToReturn = {
+    firstName: "Ivan",
+    lastName: "Ivanovich",
+    nationality: "Russian"
+  };
+  // Insira o retorno da função `getUser`
+  return callback(userToReturn);
+};
+
+console.log(getUser(userFullName)); // complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich"
+console.log(getUser(userNationality)); // complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian" */
+
+/* const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const getUser = (callback) => {
+  setTimeout(() => {
+    const user = {
+      firstName: "Ivan",
+      lastName: "Ivanovich",
+      nationality: "Russian",
+    };
+    // Retorne a `callback` passada como parâmetro na função `getUser`
+    // Dica: você pode manter o `console.log()`
+    console.log(callback(user));
+    return callback(user);
+  }, delay());
+
+};
+
+getUser(userFullName); // deve imprimir "Hello! My name is Ivan Ivanovich" depois de um certo tempo
+getUser(userNationality); // deve imprimir "Ivan is Russian" depois de um certo tempo */
+
+// --> Lidando com erros em operações assíncronas 
+
+/* const countryName = ({ name }) => console.log(`Returned country is ${name}`);
+const countryCurrency = ({ name, currency }) => console.log(`${name}'s currency is the ${currency}`);
+
+const delay = (maxMilliseconds = 5000) => Math.floor(Math.random() * maxMilliseconds);
+
+const printErrorMessage = (error) => console.log(`Error getting country: ${error}`);
+
+const getCountry = (onSuccess, onError) => {
+  setTimeout(() => {
+    const didOperationSucceed = Math.random() >= 0.5;
+    // console.log(didOperationSucceed);
+    if(didOperationSucceed) {
+      const country = {
+        name: "Brazil",
+        hdi: 0.759,
+        currency: "Real",
+      };
+      onSuccess(country);
+    } else {
+      const errorMessage = "Country could not be found";
+      onError(errorMessage);
+    }
+  }, delay());
+};
+
+// Deve imprimir "Returned country is Brazil" no sucesso ou "Error getting country: Country could not be found" em caso de falha
+getCountry(countryName, printErrorMessage);
+
+// Deve imprimir "Brazil's currency is the Real" no sucesso, ou "Error getting country: Country could not be found" em caso de falha
+getCountry(countryCurrency, printErrorMessage); */
+
+// --> Testes assíncronos com callbacks
+
+const asyncSum = (a, b, callback) => {
+  setTimeout(() => {
+    const result = a + b;
+    callback(result);
+  }, 500);
+};
+
+console.log(asyncSum(5, 10, (result)))
+
+module.exports = asyncSum;
+
+
+// --> Setup e Teardown
+
