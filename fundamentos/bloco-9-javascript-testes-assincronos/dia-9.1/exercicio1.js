89490c82430b7e6e243a18aa2e69a405dff056ea
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-01-28 21:41:11 -0300

    questao 6

--- fundamentos/bloco-9-javascript-testes-assincronos/dia-9.1/exercicio1.js
+++ fundamentos/bloco-9-javascript-testes-assincronos/dia-9.1/exercicio1.js
@@ -63,3 +63,42 @@ sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 5
 /* ------------------------------------------------------------------------------------------------------------- */
 
 
+/* const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+}
+
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * 9/5) + 32;
+
+const temperatureInFahrenheit = (temperature) =>
+  console.log(`It is currently ${toFahrenheit(temperature)}ºF at Mars`);
+
+const greet = (temperature) =>
+  console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
+
+const handleError = (errorReason) =>
+  console.log(`Error getting temperature: ${errorReason}`);
+
+// definição da função sendMarsTemperature...
+
+const sendMarsTemperature = (callback, erro) => {
+  const temperatura = getMarsTemperature();
+  const probability = Math.random();
+  setTimeout( () => {
+    if (probability < 0.6) {
+      callback(temperatura);
+    } else {
+      erro('Estou muito ocupado agora');
+    }
+  }, messageDelay()); 
+}
+
+// imprime "It is currently 47ºF at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+sendMarsTemperature(temperatureInFahrenheit, handleError);
+
+// imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo, ou "Error getting temperature: Robot is busy"
+sendMarsTemperature(greet, handleError); */
+
+/* ------------------------------------------------------------------------------------------------------------- */
