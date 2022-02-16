8415f7044c2ab01ddffb0ed7e336f6b56708a9d0
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-02-16 13:09:29 -0300

    exercicios 2 e 3

--- front-end/bloco-10-introducao-ao-react/dia-1/hello-world/src/App.js
+++ front-end/bloco-10-introducao-ao-react/dia-1/hello-world/src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+import './App.css'
 
-const Task = (value, index) => <li key={index}>{value}</li>
+/* const Task = (value, index) => <li key={index}>{value}</li>
 
 const commitment = ['learn React', 'pratic with exercise', 'read documentation'];
 
@@ -12,6 +16,16 @@ class App extends React.Component {
       </ul>
     )
   }
+} */
+
+function App() {
+  return (
+    <div className="container">
+      <Header></Header>
+      <Content></Content>
+      <Footer></Footer>
+    </div>
+  )
 }
 
 export default App
